c5a635d87ccd514710b72faae6e996963b43062b
19BEC4130 <103986326+19BEC4130@users.noreply.github.com>
2022-04-19 15:37:47 +0530

    Add files via upload

---
+++ Question #9.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#Question 9
+from scipy import stats
+a=stats.poisson.cdf(80,76)
+print("probability of pizza sales exceeds 80 in a day=",a)
+
+
+# In[ ]:
+
+
+
+
